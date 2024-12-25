13f60e480ad6940a3882a1acbf0ed99825760718
Yonas Mersha <YMersha@uneca.ad>
2024-12-25 18:53:48 +0300

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Leveraging Deep Neural Networks for Drought Forecasting',
+    author='yonas',
+    license='MIT',
+)
